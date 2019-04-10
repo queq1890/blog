8c2aac9b753c254ed1a966a607b4df7e58930710
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-04-10 17:21:59 +0900

    set index OGP image

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `queq1890 blog`,
+        short_name: `queq1890`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
