773d7759c0cf1b4cbe3eab97714f43cd51475fca
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-05-04 11:38:11 +0700

    Fix auto-fixable problems

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,8 @@
-import React from "react"
-import styled, { createGlobalStyle } from "styled-components"
-import NetlifyIdentity from "./NetlifyIdentity"
-import { Link } from "gatsby"
-import { rhythm, scale } from "../utils/typography"
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Link } from 'gatsby';
+import NetlifyIdentity from './NetlifyIdentity';
+import { rhythm, scale } from '../utils/typography';
 
 const GlobalStyle = createGlobalStyle`
 h1, h2, h3, h4 {
@@ -21,50 +21,51 @@ p code.language-text {
   padding-left: .5em;
   padding-right: .5em;
 }
-`
+`;
 
 const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
   max-width: ${rhythm(28)};
   padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
-`
+`;
 
 const RootHeader = styled.h1`
   ${scale(1.5)};
   margin-bottom: rhythm(1.5);
   margin-top: 0;
-`
+`;
 
 const PostHeader = styled.h3`
   margin-top: 0;
-`
+`;
 
 const StyledLink = styled(Link)`
   box-shadow: none;
   text-decoration: none;
   color: inherit;
-`
+`;
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
+    const { location, title, children } = this.props;
+    const rootPath = `${__PATH_PREFIX__}/`;
+    let header;
 
     if (location.pathname === rootPath) {
       header = (
         <RootHeader>
-          <StyledLink to={`/`}>{title}</StyledLink>
+          <StyledLink to="/">{title}</StyledLink>
         </RootHeader>
-      )
+      );
     } else {
       header = (
         <PostHeader>
-          <StyledLink to={`/`}>{title}</StyledLink>
+          <StyledLink to="/">{title}</StyledLink>
         </PostHeader>
-      )
+      );
     }
+
     return (
       <>
         <GlobalStyle />
@@ -75,8 +76,8 @@ class Layout extends React.Component {
           <footer />
         </Container>
       </>
-    )
+    );
   }
 }
 
-export default Layout
+export default Layout;
--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const origin = "https://amazing-bassi-eb897e.netlify.com"
+const origin = 'https://amazing-bassi-eb897e.netlify.com';
 
 function SEO({ description, lang, meta, keywords, title, image }) {
   const { site } = useStaticQuery(
@@ -25,9 +25,9 @@ function SEO({ description, lang, meta, keywords, title, image }) {
         }
       }
     `
-  )
+  );
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
@@ -38,59 +38,59 @@ function SEO({ description, lang, meta, keywords, title, image }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
-          name: `description`,
+          name: 'description',
           content: metaDescription,
         },
         {
-          property: `og:title`,
+          property: 'og:title',
           content: title,
         },
         {
-          property: `og:description`,
+          property: 'og:description',
           content: metaDescription,
         },
         {
-          property: `og:type`,
-          content: `website`,
+          property: 'og:type',
+          content: 'website',
         },
-        { property: "og:image", content: `${origin}/${image}` },
+        { property: 'og:image', content: `${origin}/${image}` },
         {
-          name: `twitter:card`,
-          content: `summary`,
+          name: 'twitter:card',
+          content: 'summary',
         },
         {
-          name: `twitter:creator`,
+          name: 'twitter:creator',
           content: site.siteMetadata.author,
         },
         {
-          name: `twitter:title`,
+          name: 'twitter:title',
           content: title,
         },
         {
-          name: `twitter:description`,
+          name: 'twitter:description',
           content: metaDescription,
         },
       ]
         .concat(
           keywords.length > 0
             ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
+                name: 'keywords',
+                content: keywords.join(', '),
               }
             : []
         )
         .concat(meta)}
     />
-  )
+  );
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: 'en',
   meta: [],
   keywords: [],
-  description: ``,
-  image: "",
-}
+  description: '',
+  image: '',
+};
 
 SEO.propTypes = {
   description: PropTypes.string,
@@ -99,6 +99,6 @@ SEO.propTypes = {
   keywords: PropTypes.arrayOf(PropTypes.string),
   title: PropTypes.string.isRequired,
   image: PropTypes.string,
-}
+};
 
-export default SEO
+export default SEO;
