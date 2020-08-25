5c3e13542a5755a0a9b03152eea1e9e0df45b3c6
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-08-25 12:22:46 +0700

    Revert "Update index.tsx"
    This reverts commit effaba8f38e475bc8155b381b41f4d47e41ffa18.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,15 @@
-import React from "react";
-import { Link, graphql } from "gatsby";
+import React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Bio from "../components/bio";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import { rhythm } from "../utils/typography";
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { rhythm } from '../utils/typography';
 
 interface Props {
   data: GatsbyTypes.BlogIndexQuery;
   location: Location;
 }
-
-const onClick = async () => {
-  const data = await fetch("https://vimeo.com/api/v2/video/52187103.json")
-    .then((res) => res.json())
-    .then((data) => data);
-
-  console.log(data);
-};
 
 const BlogIndex: React.FC<Props> = ({ data, location }) => {
   const siteTitle = data?.site?.siteMetadata?.title;
@@ -25,11 +17,9 @@ const BlogIndex: React.FC<Props> = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <button onCLick={onClick}>aaaa</button>
-      <img src="https://img.youtube.com/vi/pcNrFB9HAA4/0.jpg" alt="aaa" />
       <SEO
         title="All posts"
-        keywords={["blog", "gatsby", "javascript", "react"]}
+        keywords={['blog', 'gatsby', 'javascript', 'react']}
         image="/images/profile-pic.jpg"
       />
       <Bio />
@@ -44,7 +34,7 @@ const BlogIndex: React.FC<Props> = ({ data, location }) => {
                   marginBottom: rhythm(1 / 4),
                 }}
               >
-                <Link style={{ boxShadow: "none" }} to={node?.fields?.slug}>
+                <Link style={{ boxShadow: 'none' }} to={node?.fields?.slug}>
                   {title}
                 </Link>
               </h3>
