8bb22af959aa0052f6e539dadb913d1bc2175e4d
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-08-25 12:22:44 +0700

    Revert "Update index.tsx"
    This reverts commit 7725ec9b0a1e92ac4f9e8494b3a4c5b05c83a3bd.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,21 @@ interface Props {
   location: Location;
 }
 
+const onClick = async () => {
+  const data = await fetch("https://vimeo.com/api/v2/video/52187103.json")
+    .then((res) => res.json())
+    .then((data) => data);
+
+  console.log(data);
+};
+
 const BlogIndex: React.FC<Props> = ({ data, location }) => {
   const siteTitle = data?.site?.siteMetadata?.title;
   const posts = data.allMarkdownRemark.edges;
 
   return (
     <Layout location={location} title={siteTitle}>
-      <button onClick={onClick}>aaaa</button>
+      <button onCLick={onClick}>aaaa</button>
       <img src="https://img.youtube.com/vi/pcNrFB9HAA4/0.jpg" alt="aaa" />
       <SEO
         title="All posts"
